d045f5e8e815529c58d43ec31ea10f04649c47c0
Yiyang Li <henryly94@gmail.com>
2018-07-02 15:11:10 -0400

    Initialize CMake Project Structure

---
+++ src/Common/utils.cpp
@@ -0,0 +1,10 @@
+#include "utils.hpp"
+#include <iostream>
+using namespace std;
+
+void print(){
+	for (int i=0; i<10; i++){
+		cout << i << ' ';
+	}	
+	cout << endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "utils.hpp"
+
+int main(){
+	
+	std::cout << "Hello World" << std::endl;
+	print();
+	return 0;
+}
